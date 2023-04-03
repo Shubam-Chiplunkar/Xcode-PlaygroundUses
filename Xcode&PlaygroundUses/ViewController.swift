cd2b59fc23b048c7e69edf4ff2ebc84ad851e943
Shubham Chiplunkar <shubhamchiplunkar21@gmail.com>
2023-04-03 11:20:55 +0530

    Checklist Task of Xcode and Playground Uses, Displaying array of words in tableview in uppercase

--- Xcode&PlaygroundUses/ViewController.swift
+++ Xcode&PlaygroundUses/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    let arrayOfWords : [String] = ["hello","world","table","football","pipe","code"]
+    var wordsInTitle = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var str : String = ""
+        for i in arrayOfWords{
+            str = i.uppercased()
+            wordsInTitle.append(str)
+        }
     }
-
 
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wordsInTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = wordsInTitle[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
